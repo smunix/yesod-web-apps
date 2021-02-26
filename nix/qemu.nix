1651797ca0ff470a9d83f76ca1eac72aa253d5dd
Providence Salumu <Providence.Salumu@smunix.com>
2021-02-26 08:17:38 -0500

    qemu, configure service user

--- nix/qemu.nix
+++ nix/qemu.nix
@@ -10,28 +10,48 @@ with haskell.lib;
       inherit system;
       configuration = { config, pkgs, ... }:
         with (import ./overlays.nix { inherit system version; } final prev);
-        {
-          networking.hostName = "festhest";
-          networking.firewall.allowedTCPPorts = [ 22 80 ];
-          environment.systemPackages = [ apps.festhest ];
-          systemd.services.festhest = {
-            description = "festhest Webserver";
-            wantedBy = [ "multi-user.agent" ];
-            serviceConfig = {
-              ExecStart = "${apps.festhest}/bin/festhest";
+        let user = "festhest"
+        in
+          {
+            networking.hostName = user;
+            networking.firewall.allowedTCPPorts = [ 22 80 ];
+            environment.systemPackages = [ apps.festhest ];
+            systemd.services.festhest = {
+              description = "festhest Webserver";
+              wantedBy = [ "default.target" ];
+              after = [ "network.target" ];
+              serviceConfig = {
+                Type = "forking";
+                User = "root";
+                ExecStart = "${apps.festhest}/bin/festhest";
+              };
             };
-          };
-          users = {
-            mutableUsers = false;
             users = {
-              root.password = "";
-              "festhest".isSystemUser = true;
+              mutableUsers = false;
+              users = {
+                root = {
+                  password = "";
+                };
+                "${user}" = {
+                  isSystemUser = true;
+                  password = "";
+                };
+              };
+              extraUsers = {
+                "${user}" = {
+                  group = "wheel";
+                  shell = pkgs.fish;
+                };
+              };
+            };
+            security.sudo = {
+              enable = true;
+              wheelNeedsPassword = false;
+            };
+            virtualisation = {
+              graphics = false;
             };
           };
-          virtualisation = {
-            graphics = false;
-          };
-        };
     }).vm; 
   });
 }
