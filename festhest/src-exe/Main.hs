24b914c8799dad2ce7b32a5b8ea883a1ea3bd2cb
Providence Salumu <Providence.Salumu@smunix.com>
2021-02-26 06:48:53 -0500

    qemu, initial setup

--- festhest/src-exe/Main.hs
+++ festhest/src-exe/Main.hs
@@ -7,9 +7,11 @@ data App = App
 instance Yesod App
 
 mkYesod "App" [parseRoutes| HomeR GET |]
-  
+
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet| <h1>Hello, Haskell funs! |]
-  
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Starting Warp 80 App..."
+  warp 80 App
--- nix/qemu.nix
+++ nix/qemu.nix
@@ -6,12 +6,24 @@ with haskellPackages;
 with haskell.lib;
 {
   qemu = recurseIntoAttrs ({
-    apps = import ./overlays.nix { inherit system version; } final prev;
+    
     vm = (import "${nixpkgs}/nixos" {
       inherit system;
-      configuration = { config, pkgs, ... }: {
-        
-      };
+      configuration = { config, pkgs, ... }:
+        with (import ./overlays.nix { inherit system version; } final prev);
+        {
+          networking.hostName = "festhest";
+          networking.firewall.allowedTCPPorts = [ 22 80 ];
+          environment.systemPackages = [ apps.festhest ];
+          systemd.services.festhest = {
+            description = "festhest Webserver";
+            wantedBy = [ "multi-user.agent" ];
+            serviceConfig = {
+              ExecStart = "${apps.festhest}/bin/festhest";
+            };
+          };
+          
+        };
     }).vm; 
   });
 }
