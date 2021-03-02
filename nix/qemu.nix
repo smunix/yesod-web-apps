084758dfc3f906cfa6bb5801d7d7863b90bc9627
Providence Salumu <Providence.Salumu@smunix.com>
2021-03-02 08:59:44 -0500

    qemu: cores set to 4 and add htop

--- nix/qemu.nix
+++ nix/qemu.nix
@@ -75,7 +75,7 @@ in
             networking.hostName = hostname;
             networking.firewall.allowedTCPPorts = [ 22 3080 3000 ];
             environment.systemPackages =
-              []
+              (with pkgs; [ htop ])
               ++ (mkSystemPackages { name = "festhest"; })
               ++ (mkSystemPackages { name = "amahoro"; })
             ;
@@ -99,10 +99,10 @@ in
             };
             virtualisation = {
               graphics = false;
+              cores = 4;
               # https://wiki.qemu.org/Documentation/Networking#Network_Basics
               qemu.networkingOptions = [
-                # "-net nic,netdev=user.0,model=virtio"
-                "-device virtio-net-pci,netdev=user.0 "
+                "-device virtio-net-pci,netdev=user.0"
                 "-netdev type=user,id=user.0\${QEMU_NET_OPTS:+,$QEMU_NET_OPTS},hostfwd=tcp::3080-:3080,hostfwd=tcp::3000-:3000"
               ];
             };
