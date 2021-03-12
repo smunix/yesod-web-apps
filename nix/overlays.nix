48d757473527f28e36a8b49e4614d6ad59e801cc
Providence Salumu <Providence.Salumu@smunix.com>
2021-03-12 18:27:40 -0500

    festhest: monad random

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -4,9 +4,11 @@ _:
 with final;
 with haskellPackages;
 with haskell.lib;
+let random = callHackage "random" "1.2.0" {};
+in
 {
   apps = recurseIntoAttrs ({
-    festhest = overrideCabal (addBuildTool (callCabal2nix "festhest" ../festhest {}) yesod-bin) (o: { version = o.version + "-" + version; });
+    festhest = overrideCabal (addBuildTool (callCabal2nix "festhest" ../festhest { inherit random; }) yesod-bin) (o: { version = o.version + "-" + version; });
     amahoro = overrideCabal (addBuildTool (callCabal2nix "amahoro" ../amahoro {}) yesod-bin) (o: { version = o.version + "-" + version; });
     amakuru = overrideCabal (addBuildTool (callCabal2nix "amakuru" ../amakuru {}) yesod-bin) (o: { version = o.version + "-" + version; });
   });
