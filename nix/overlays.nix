84441a9e1d57a4c0370285c933cee2a089e96df9
Providence Salumu <Providence.Salumu@smunix.com>
2021-03-13 05:15:50 -0500

    random: bump version -> 1.2.0

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,14 +1,14 @@
 { system, version, ... }:
-final:
-_:
-with final;
-with haskellPackages;
+self:
+super:
+with self;
+with super.haskellPackages.extend(hself: hsuper: {
+  random = super.haskellPackages.callHackage "random" "1.2.0" {};
+});
 with haskell.lib;
-let random = callHackage "random" "1.2.0" {};
-in
 {
   apps = recurseIntoAttrs ({
-    festhest = overrideCabal (addBuildTool (callCabal2nix "festhest" ../festhest { inherit random; }) yesod-bin) (o: { version = o.version + "-" + version; });
+    festhest =overrideCabal (addBuildTool (callCabal2nix "festhest" ../festhest {}) yesod-bin) (o: { version = o.version + "-" + version; });
     amahoro = overrideCabal (addBuildTool (callCabal2nix "amahoro" ../amahoro {}) yesod-bin) (o: { version = o.version + "-" + version; });
     amakuru = overrideCabal (addBuildTool (callCabal2nix "amakuru" ../amakuru {}) yesod-bin) (o: { version = o.version + "-" + version; });
   });
